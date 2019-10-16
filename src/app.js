42cb2692318ee91134900fc23cd59aceb182b138
Mohit nagori <nagorimohit21@gmail.com>
2019-10-16 19:48:33 +0530

    Heroku config

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fetchProducts = require('./api/products');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/api/products', (req, res) => {
   fetchProducts(req.query.filter, (error, products) => {
@@ -14,6 +15,15 @@ app.get('/api/products', (req, res) => {
   })
 });
 
-app.listen(5000, () => {
-  console.log('Server is up on port 5000');
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', () => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`);
 });
